5f2d97248a79e840cfae0bec112e90b879249905
Harun Günlü <gue@christmann.info>
2018-02-28 09:55:35 +0100

    add newAction, edit generatemap.php, move notes

--- bla.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// test um zu gucken wie die Daten abgespeichert werden
-$score = [0 => ["steps" => 2, "time" => 3]];
-$json = json_encode($score);
-file_put_contents('highscore.txt', $json);
